0d8b72e94a84d373298d5c8d02caae455fd75931
liujiaxing7 <1445101363@qq.com>
2021-09-16 19:19:16 +0800

    update

---
+++ xml/lighter.py
@@ -0,0 +1,39 @@
+import os
+
+import cv2
+from PIL import Image
+
+
+def get_xml(input_dir):
+    xml_path_list = []
+    for (root_path, dirname, filenames) in os.walk(input_dir):
+        for filename in filenames:
+            if filename.endswith('.png'):
+                xml_path = root_path + "/" + filename
+                xml_path_list.append(xml_path)
+    return xml_path_list
+
+
+import argparse
+from tqdm import tqdm
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--area', type=int, default="468")
+    parser.add_argument('--input_dir', type=str, default="/mnt/sdb1/Data/data3/902_0712/7_9/images/val")
+    parser.add_argument('--output_dir', type=str,
+                        default="/mnt/sdb1/Data/data3/902_0712/7_9/images/light2")
+    args = parser.parse_args()
+
+    xml_path_list = get_xml(args.input_dir)
+    for xml_file in tqdm(xml_path_list):
+        im1 = Image.open(xml_file)
+
+        im2 = im1.point(lambda p: p * 2.5)
+
+        output_file = os.path.join(args.output_dir, xml_file[len(args.input_dir) + 1:])
+        if not os.path.exists(os.path.dirname(output_file)):
+            os.makedirs(os.path.dirname(output_file))
+
+        im2.save(output_file)
