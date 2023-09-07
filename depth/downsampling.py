4d3e4b760e7f3413a1ee8760dcdc1b19e62a6582
ljx <1445101363@qq.com>
2023-09-07 14:10:11 +0800

    update downsample

--- depth/downsampling.py
+++ depth/downsampling.py
@@ -2,6 +2,7 @@ import argparse
 import re
 
 import cv2
+import numpy as np
 from PIL import Image
 import os
 
@@ -31,18 +32,20 @@ def resize_images_in_folder(input_path, output_path, scale):
     # 遍历每一张图片，并将其resize成原来的2倍大小
     for img_file in file_list:
         # 打开图片文件
-        img = cv2.imread(img_file)
+        img = cv2.imread(img_file, -1)
 
         # 计算新的图片大小
         resized_image = img[::2, ::2]
+        resized_image = resized_image / 2
 
         # 规定输出文件名
-        output_filename = img_file.replace("data_2023_08_23_crop", "data_2023_08_23_crop_288_190")
+        output_filename = img_file.replace("scale_tof_crop_disp", "scale_tof_crop_disp_288_180")
 
         if not os.path.exists(os.path.split(output_filename)[0]):
             os.makedirs(os.path.split(output_filename)[0])
 
         # 保存resize后的图片
+        resized_image = np.array(resized_image).astype("uint16")
         cv2.imwrite(output_filename, resized_image)
 
 # 创建命令行参数解析器
